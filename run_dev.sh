3dbc269e3dd0af1b659c9f406743848399453d1d
dertyp7 <dertyp7@tealfire.de>
2024-01-18 21:34:50 +0100

    Update nodemon command in run_dev.sh

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,6 @@
 trap "bash clean_dev_env.sh" INT
 
-npx nodemon -w ./src --ext scss --exec sh -- build_dev.sh
+npx nodemon -L -w ./src --ext scss --exec sh -- build_dev.sh
 
 bash clean_dev_env.sh
 
